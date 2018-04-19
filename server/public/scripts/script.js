53d2cdb358e48dba85d1a7431f63a1dec82d2fb7
bree melechinsky <groundcontroltomajorbree@gmail.com>
2018-04-18 22:17:27 -0500

    add public files connecting to server and me spending too much time learning how to locally source custom fonts. plus js and jquery check

---
+++ server/public/scripts/script.js
@@ -0,0 +1,7 @@
+console.log('js czech');
+
+$(document).ready(onReady);
+
+function onReady()  {
+    console.log('jquery czech');
+}
--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const monsters = require('./modules/monsters.js');
+
 app.use(express.static('server/public'));
 
+app.get('/monsters', (req, res) => {
+    res.send(monsters);
+});
 
 app.listen(PORT, () =>  {
 console.log(`listening in port: ${PORT}`);
